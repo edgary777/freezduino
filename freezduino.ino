5463e496cc4e6c8266f882cdf5125b32f42136f6
edgary777 <edgar21dejunio@gmail.com>
2018-03-08 16:49:39 -0600

    Bug fix and custom setpoint control.
    Bug Fix:
    - On button1, dirtyMeny was set to true outside the if so it was being
    	caught all the time instead of only when it was clicked and it
    	led to the lcd being refreshed every loop.
    New Features:
    - The setpoint config submenu is now accessible and functional.
    	it allows the user to set a new setpoint on the fly.

--- freezduino.ino
+++ freezduino.ino
@@ -1,22 +1,22 @@
 /**
- * Name     : Freezduino
- * Author   : Edgar Solis Vizcarra
- * Date     : 2018/03/04
- * Modified : 2018/03/05
- * Version  : V0.1.2
- * Notes    : Sketch designed for the control of a walk-in freezer evaporator.
- *    It takes input from a temperature sensor and uses it to control
- *    3 relays.
- *    relay1 controls the solenoid valve on the evaporator that makes the
- *    freezer compressor turn on and off.
- *    relay2 controls the fans that push air throught the freezer diffusor.
- *    relay3 controls the heating elements that defrost the diffusor at the
- *    time intervals set by the user.
- *
- *    A 16x2 LCD display and 3 buttons are used for user interaction.
- *
- *    There's also an on/off button
- */
+   Name     : Freezduino
+   Author   : Edgar Solis Vizcarra
+   Date     : 2018/03/04
+   Modified : 2018/03/05
+   Version  : V0.1.2
+   Notes    : Sketch designed for the control of a walk-in freezer evaporator.
+      It takes input from a temperature sensor and uses it to control
+      3 relays.
+      relay1 controls the solenoid valve on the evaporator that makes the
+      freezer compressor turn on and off.
+      relay2 controls the fans that push air throught the freezer diffusor.
+      relay3 controls the heating elements that defrost the diffusor at the
+      time intervals set by the user.
+
+      A 16x2 LCD display and 3 buttons are used for user interaction.
+
+      There's also an on/off button
+*/
 
 #include <LiquidCrystal.h>
 #include <Wire.h>
@@ -62,6 +62,20 @@ bool menuOn = false;
 // Signals when the home screen needs to be cleaned.
 bool homeClean = true;
 
+//indicates the currently active submenu, 0 = no active submenu.
+int activeSubMenu = 0;
+
+// This variable is shared by all the submenus, it signals the current selection of the submenu.
+int subMenuCurrent = 0;
+
+// Temporary setpoint copy storage, this copy is the one you interact with the screen and the real setpoint is set after you leave.
+int newSetpoint = setpoint;
+
+// When there's a change in a submenu this sets it to be updated.
+bool dirtySubMenu = true;
+
+// Sets the data of this submenu to be updated
+int dirtyConfig;
 
 // initialize the temp sensor library, as it connects via I2C the pins are
 // defined in the library. SCL = A5 on UNO || D21 on MEGA SDA = A4 on UNO || D20
@@ -76,7 +90,7 @@ byte thermometer[8] = {
   0b01010,
   0b01110,
   0b10111,
-  0b11111, 
+  0b11111,
   0b01110
 };
 
@@ -172,15 +186,15 @@ void setup() {
 }
 //This controls how often we check for input from the buttons first value is in ms second is callback function
 TimedAction lcdTempThread = TimedAction(refreshRate, lcdTemp);
-TimedAction lcdDefrosterThread = TimedAction(refreshRate / 2,lcdDefrost);
-TimedAction lcdEvaporatorThread = TimedAction(refreshRate / 2,lcdEvaporator);
+TimedAction lcdDefrosterThread = TimedAction(refreshRate / 2, lcdDefrost);
+TimedAction lcdEvaporatorThread = TimedAction(refreshRate / 2, lcdEvaporator);
 
 void tempControl(int setpoint, int tolerance) {
   /**
-   * Check temperature, if it is at the setpoint or lower, or higher but within
-   * the tolerance then turn relay off. If it is higher than the setpoint + the
-   * tolerance then turn the relay on.
-   */
+     Check temperature, if it is at the setpoint or lower, or higher but within
+     the tolerance then turn relay off. If it is higher than the setpoint + the
+     tolerance then turn the relay on.
+  */
   int temp = round(tempsensor.readTempC()); // round temperature reading.
   if (temp <= setpoint + tolerance) {
     digitalWrite(Rcompressor, LOW);
@@ -195,32 +209,32 @@ void tempControl(int setpoint, int tolerance) {
 
 void defrost(bool order = false) {
   /**
-   * Activate the defrosting function.
-   *
-   * When defrosting is activated the compressor and the fans are turned off
-   * and the heating element is turned on.
-   * it must stay like this for the set duration or until the user decides to
-   * cancel the defrosting.
-   */
-   if(order == true) {
+     Activate the defrosting function.
+
+     When defrosting is activated the compressor and the fans are turned off
+     and the heating element is turned on.
+     it must stay like this for the set duration or until the user decides to
+     cancel the defrosting.
+  */
+  if (order == true) {
     digitalWrite(Rdefroster, HIGH);
     defroster = true;
-   }
-   else {
+  }
+  else {
     defroster = false;
     digitalWrite(Rdefroster, LOW);
-   }
+  }
 }
 
-void lcdEvaporator(int row, int column){
+void lcdEvaporator(int row, int column) {
   /**
-   * Write the current state of the evaporator on the lcd display.
-   * 
-   * It is written LTR starting on the selected row and column.
-   * it displays either ON or OFF
-   * 
-   * it needs 4 spaces.
-   */
+     Write the current state of the evaporator on the lcd display.
+
+     It is written LTR starting on the selected row and column.
+     it displays either ON or OFF
+
+     it needs 4 spaces.
+  */
   lcd.setCursor(lcdEvaporatorColumn, lcdEvaporatorRow);
   lcd.write(byte(2));
   if (evaporator == false) {
@@ -231,15 +245,15 @@ void lcdEvaporator(int row, int column){
   }
 }
 
-void lcdDefrost(){
+void lcdDefrost() {
   /**
-   * Write the current state of the defroster on the lcd display.
-   * 
-   * It is written LTR starting on the selected row and column.
-   * it displays either ON or OFF
-   * 
-   * it needs 4 spaces.
-   */
+     Write the current state of the defroster on the lcd display.
+
+     It is written LTR starting on the selected row and column.
+     it displays either ON or OFF
+
+     it needs 4 spaces.
+  */
   lcd.setCursor(lcdDefrosterColumn, lcdDefrosterRow);
   lcd.write(byte(3));
   if (defroster == false) {
@@ -252,14 +266,14 @@ void lcdDefrost(){
 
 void lcdTemp() {
   /**
-   * Write the temperature reading from the sensor to the lcd display.
-   *
-   * It is written LTR starting on the selected row and column.
-   * By default the temperature is rounded, if precise is set to true it is not
-   * rounded and it shows 2 decimals.
-   * 
-   * It needs 3 spaces when not precise,  when precise it needs 6
-   */
+     Write the temperature reading from the sensor to the lcd display.
+
+     It is written LTR starting on the selected row and column.
+     By default the temperature is rounded, if precise is set to true it is not
+     rounded and it shows 2 decimals.
+
+     It needs 3 spaces when not precise,  when precise it needs 6
+  */
   checkTempSensor();
   lcd.setCursor(lcdTempColumn, lcdTempRow); // set the cursor to the top row, 1st position
   lcd.write(byte(0));         // thermometer character
@@ -280,10 +294,10 @@ void lcdTemp() {
 
 void checkTempSensor() {
   /**
-   * Check if the temp sensor is connected.
-   * 
-   * If it is connected do nothing, if it is not connected stop everything.
-   */
+     Check if the temp sensor is connected.
+
+     If it is connected do nothing, if it is not connected stop everything.
+  */
 
   if (!tempsensor.begin()) {
     lcd.clear();
@@ -297,30 +311,30 @@ void checkTempSensor() {
   }
 }
 
-void killAll(){
+void killAll() {
   /**
-   * Kill all processes and show an alert message.
-   */
+     Kill all processes and show an alert message.
+  */
 }
 
-void updateTempPos(int column, int row, bool precise){
+void updateTempPos(int column, int row, bool precise) {
   lcdTempColumn = column;
   lcdTempRow = row;
   lcdTempPrecise = precise;
 }
 
-void updateEvaporatorPos(int column, int row){
+void updateEvaporatorPos(int column, int row) {
   lcdEvaporatorColumn = column;
   lcdEvaporatorRow = row;
 }
 
-void updateDefrosterPos(int column, int row){
+void updateDefrosterPos(int column, int row) {
   lcdDefrosterColumn = column;
   lcdDefrosterRow = row;
 }
 
-void homeScreen(){
-  updateTempPos(0,0,true);
+void homeScreen() {
+  updateTempPos(0, 0, true);
   updateEvaporatorPos(12, 0);
   updateDefrosterPos(0, 1);
   lcdTempThread.check();
@@ -328,36 +342,46 @@ void homeScreen(){
   lcdDefrosterThread.check();
 }
 
-void setpointConfig(){
+void setpointConfig() {
   /*
-   * Display and interact with the temperature setpoint on the LCD display.
-   * 
-   * 
-   */
+     Display and interact with the temperature setpoint on the LCD display.
+
+     newSetpoint is a temporary copy of the real setpoint, the real setpoint is only updated
+     after you leave the menu so you don't change the temp in real time.
+  */
+  if (dirtySubMenu == true) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.write(byte(0));
+    lcd.print(newSetpoint);
+    lcd.write(byte(1));
+    lcd.print("C");
+    dirtySubMenu = false;
+  }
 }
 
-void menuScreen(int top, int current){
+void menuScreen(int top, int current) {
   /**
-   * Menu with options to change settings.
-   * 
-   * parameters:
-   * - top: signals the message that is on the top of the screen.
-   * - current: signals the message that is currently selected.
-   * 
-   * We always start with 0, 0
-   * 
-   * 0- return
-   * 1- setpoint
-   * 2- tolerance
-   * 3- defrost time
-   * 4- toggle fans
-   * 5- toggle evaporator
-   * 6- toggle defroster
-   * 7- shutdown
-   */
+     Menu with options to change settings.
+
+     parameters:
+     - top: signals the message that is on the top of the screen.
+     - current: signals the message that is currently selected.
+
+     We always start with 0, 0
+
+     0- return
+     1- setpoint
+     2- tolerance
+     3- defrost time
+     4- toggle fans
+     5- toggle evaporator
+     6- toggle defroster
+     7- shutdown
+  */
   if (dirtyMenu == true) {
     char message [8][15] = {"REGRESAR", "TEMPERATURA", "TOLERANCIA", "TIEMPO DESCONG", "VENTILADORES", "DIFUSOR", "DESCONGELAR", "APAGAR"};
-  
+
     lcd.clear();
     lcd.setCursor(1, 0);
     lcd.print(message[top]);
@@ -371,27 +395,162 @@ void menuScreen(int top, int current){
 }
 
 
-void button1(){
+void button1() {
   /**
-   * This button does many things.
-   * 1- Get in the mainMenu from the home screen
-   * 2- Act as a click to interact with the menus and submenus options inside.
-   * If button is pressed deactivate for 200 ms.
-   */
-  if (b1Millis <= millis()){
-    if (digitalRead(b1Pin) == HIGH){
+     This button does many things.
+     1- Get in the mainMenu from the home screen
+     2- Act as a click to interact with the menus and submenus options inside.
+     If button is pressed deactivate for 200 ms.
+  */
+  if (b1Millis <= millis()) {
+    if (digitalRead(b1Pin) == HIGH) {
       b1Millis = millis() + 500;
-      if (menuOn == false){
-        menuOn = !menuOn;        
-      }
-      else{
-        switch(mainMenuCurrent) {
+      if (activeSubMenu == 0){
+        switch (mainMenuCurrent) {
           case 0:
             // RETURN TO HOME
             menuOn = !menuOn;
             break;
           case 1:
             // SETPOINT CONFIG
+            activeSubMenu = 1;
+            newSetpoint = setpoint;
+            break;
+          case 2:
+            // TOLERANCE CONFIG
+            break;
+          case 3:
+            // DEFROST CONFIG
+            break;
+          case 4:
+            // FORCE FANS ON/OFF
+            break;
+          case 5:
+            // FORCE EVAPORATOR ON/OFF
+            break;
+          case 6:
+            // FORCE DEFROST ON/OFF
+            break;
+          case 7:
+            // SHUTDOWN
+            break;
+        }
+      }
+      else{
+        switch (activeSubMenu) {
+          case 1:
+            // INTERACT WITH SETPOINT CONFIG
+            setpoint = newSetpoint;
+            activeSubMenu = 0;
+            dirtySubMenu = true;
+            break;
+          case 2:
+            // INTERACT WITH TOLERANCE CONFIG
+            break;
+          case 3:
+            // INTERACT WITH DEFROST CONFIG
+            break;
+          case 4:
+            // FORCE FANS ON/OFF
+            break;
+          case 5:
+            // FORCE EVAPORATOR ON/OFF
+            break;
+          case 6:
+            // FORCE DEFROST ON/OFF
+            break;
+          case 7:
+            // SHUTDOWN
+            break;
+        }
+      }
+      dirtyMenu = true;
+    }
+  }
+}
+
+void button2() {
+  /**
+     This button moves down the current menu.
+
+     If button is pressed deactivate for 200 ms.
+  */
+  if (b2Millis <= millis()) {
+    if (digitalRead(b2Pin) == HIGH) {
+      b2Millis = millis() + 500;
+      if (activeSubMenu == 0) {
+        if (mainMenuCurrent - mainMenuTop != 0) {
+          if (mainMenuTop < 7) {
+            mainMenuTop = mainMenuTop + 1;
+          }
+          else {
+            mainMenuCurrent = mainMenuTop;
+          }
+        }
+        if (mainMenuTop - mainMenuCurrent == 0) {
+          mainMenuCurrent = mainMenuCurrent + 1;
+        }
+      }
+      else{
+        switch (activeSubMenu) {
+          case 1:
+            // INTERACT WITH SETPOINT CONFIG
+            newSetpoint = newSetpoint - 1;
+            dirtySubMenu = true;
+            break;
+          case 2:
+            // INTERACT WITH TOLERANCE CONFIG
+            break;
+          case 3:
+            // INTERACT WITH DEFROST CONFIG
+            break;
+          case 4:
+            // FORCE FANS ON/OFF
+            break;
+          case 5:
+            // FORCE EVAPORATOR ON/OFF
+            break;
+          case 6:
+            // FORCE DEFROST ON/OFF
+            break;
+          case 7:
+            // SHUTDOWN
+            break;
+        }
+      }
+      dirtyMenu = true;
+    }
+  }
+}
+
+void button3() {
+  /**
+     This button moves up the current menu.
+
+     If button is pressed deactivate for 200 ms.
+  */
+  if (b3Millis <= millis()) {
+    if (digitalRead(b3Pin) == HIGH) {
+      b3Millis = millis() + 500;
+      if (activeSubMenu == 0){
+        if (mainMenuCurrent - mainMenuTop == 0) {
+          if (mainMenuTop > 0) {
+            mainMenuTop = mainMenuTop - 1;
+          }
+          else {
+            mainMenuCurrent = mainMenuTop;
+          }
+        }
+        if (mainMenuTop - mainMenuCurrent != 0) {
+          mainMenuCurrent = mainMenuCurrent - 1;
+        }
+      }
+      else {
+        switch (activeSubMenu) {
+          case 1:
+            // SETPOINT CONFIG
+            newSetpoint = newSetpoint + 1;
+            dirtySubMenu = true;
             break;
           case 2:
             // TOLERANCE CONFIG
@@ -418,78 +577,47 @@ void button1(){
   }
 }
 
-void button2(){
-  /**
-   * This button moves down the current menu.
-   * 
-   * If button is pressed deactivate for 200 ms.
-   */
-  if (b2Millis <= millis()){
-    if (digitalRead(b2Pin) == HIGH){
-      b2Millis = millis() + 500;
-      if (mainMenuCurrent - mainMenuTop != 0){
-        if (mainMenuTop < 7) {
-         mainMenuTop = mainMenuTop + 1;
-        }
-        else{
-          mainMenuCurrent = mainMenuTop;
-        }
-      }
-      if (mainMenuTop - mainMenuCurrent == 0){
-        mainMenuCurrent = mainMenuCurrent + 1;
-      }
-      dirtyMenu = true;
-    }
-  }
-}
-
-void button3(){
-  /**
-   * This button moves up the current menu.
-   * 
-   * If button is pressed deactivate for 200 ms.
-   */
-  if (b3Millis <= millis()){
-    if (digitalRead(b3Pin) == HIGH){
-      b3Millis = millis() + 500;
-      if (mainMenuCurrent - mainMenuTop == 0){
-        if (mainMenuTop > 0) {
-         mainMenuTop = mainMenuTop - 1;
-        }
-        else{
-          mainMenuCurrent = mainMenuTop;
-        }
-      }
-      if (mainMenuTop - mainMenuCurrent != 0){
-        mainMenuCurrent = mainMenuCurrent - 1;
-      }
-      dirtyMenu = true;
-    }
-  }
-}
-
-void updateMenu(){
+void updateMenu() {
   /*
-   * Update the menu by pressing of the buttons.
+     Update the menu by pressing of the buttons.
+  */
+  button3();
+  button2();
+  button1();
+}
+
+void showSubMenu(){
+  /*
+   * display the actively selected submenu.
+   * 
+   * The global variable activeSubMenu controls which is shown.
    */
- button3();
- button2();
- button1();
+  switch(activeSubMenu){
+    case 1:
+    // Setpoint config.
+    setpointConfig();
+  }
 }
 
 void loop() {
   tempControl(setpoint, tolerance);
   button1();
-  if (menuOn == true){
-    homeClean = false;
-    updateMenu();
-    menuScreen(mainMenuTop, mainMenuCurrent);
+  if (menuOn == true) {
+    if (activeSubMenu == 0) {
+      homeClean = false;
+      updateMenu();
+      menuScreen(mainMenuTop, mainMenuCurrent);
+    }
+    else {
+      updateMenu();
+      showSubMenu();
+    }
   }
-  else{
-    if (homeClean == false){
+  else {
+    if (homeClean == false) {
       lcd.clear();
       homeClean = true;
     }
     homeScreen();
-  }  
+  }
 }
